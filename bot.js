f042e8a266ffb856d263e86ba08a6b2703bbf49e
jimbonlemu <iqbaltech29@gmail.com>
2023-02-09 14:23:35 +0700

    adding js function on HTML

---
+++ bot.js
@@ -0,0 +1,56 @@
+
+const question = document.getElementById('pertanyaan')
+const answer = document.getElementById('jawaban')
+
+let init = 0
+
+
+const botQuest = (data) => {
+    return [
+        "Halo aku adalah bot faker , kamu siapa namanya ?",
+        `Halo ${data?.nama} , kamu berapa usianya?`,
+        `Oh kamu  ${data?.usia} tahun , kamu apa hobinya ? `,
+        `Jadi hobimu ${data?.hobi} , kalo boleh tau udah punya cewe apa belom ?`,
+        `Oh.. ${data?.pacar}, kalo gitu gw cabut dulua yak...`
+    ]
+}
+
+question.innerHTML = botQuest()[0]
+
+let usersData = []
+
+function botStart() {
+    init++
+    if (init === 1) {
+        botDelay({ nama: answer.value })
+    } else if (init === 2) {
+        botDelay({ usia: answer.value })
+    } else if (init === 3) {
+        botDelay({ hobi: answer.value })
+    } else if (init === 4) {
+        botDelay({ pacar: answer.value })
+    } else if (init === 5) {
+        finishing()
+    } else {
+        botEnd()
+    }
+
+}
+
+function botDelay(jawabanUser) {
+    setTimeout(() => {
+        question.innerHTML = botQuest(jawabanUser)[init]
+        answer.value = ""
+    }, [1250]);
+
+    usersData.push(answer.value)
+}
+
+function finishing() {
+    question.innerHTML = `Thank you ${usersData[0]} udah main main kesini 😁 ,
+        kapan-kapan kita  ${usersData[2]}  bareng yak`
+}
+
+function botEnd() {
+    window.location.reload()
+}
